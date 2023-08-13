5eca929853e9e15bec3444be2c6daac8386e8003
Sudharsen27 <130970966+Sudharsen27@users.noreply.github.com>
2023-08-13 12:29:02 +0530

    Update Twitter.java

--- Twitter.java
+++ Twitter.java
@@ -11,11 +11,11 @@ public class Twitter {
 		WebDriver d= new ChromeDriver();
 		d.get("https://twitter.com/i/flow/login");
 		Thread.sleep(6000);
-		d.findElement(By.name("text")).sendKeys("6382519651");
+		d.findElement(By.name("text")).sendKeys("6383456666");
 		d.findElement(By.xpath("//div[@class='css-901oao r-1awozwy r-6koalj r-18u37iz r-16y2uox r-37j5jr"
 				+ " r-a023e6 r-b88u0q r-1777fci r-rjixqe r-bcqeeo r-q4m81j r-qvutc0']")).click();
 		Thread.sleep(7000);
-		d.findElement(By.name("password")).sendKeys("Sudharsen");
+		d.findElement(By.name("password")).sendKeys("12345");
 		d.findElement(By.xpath("//div[@class='css-901oao r-1awozwy r-6koalj r-18u37iz r-16y2uox"
 				+ " r-37j5jr r-a023e6 r-b88u0q r-1777fci r-rjixqe r-bcqeeo r-q4m81j r-qvutc0']")).click();
 	}
